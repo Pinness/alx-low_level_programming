c64ce456ed7e8a5a0e2265c7709fa6b877f6879c
Pinness <adamhappinessjw@gmail.com>
2022-10-27 15:50:35 -0700

    string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sring.h>
+#include "main.h"
+/**
+ * _strncat - to incatenate
+ * (short description)
+ * @dest: to incatenate
+ * @src: to incatenate
+ * @n: to be incatenated
+ * Return: character
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return dest;
+}
